faa23e9e3b4dc12328c2997c316fad47ceafef33
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-10-05 16:59:56 -0300

    fix: Adiciona o DataBaseModule ao appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './database/prisma.service';
+import { DatabaseModule } from './database/database.module';
 
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, DatabaseModule],
   controllers: [],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService],
 })
 export class AppModule {}
