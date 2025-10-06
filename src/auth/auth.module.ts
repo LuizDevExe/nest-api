c1495a305ade3c6545cab3ed906070afdda52548
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-10-05 22:32:36 -0300

    feat: Adiciona authguard e UseGuard ao user.controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,21 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
-    UserModule,
+    forwardRef(() => UserModule),
     JwtModule.register({
       global: true,
       secret: process.env.SECRET_KEY || '',
       signOptions: { expiresIn: '43200s'},
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
   controllers: [AuthController],
+  exports:[AuthGuard, AuthService]
 })
 export class AuthModule {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,8 +7,10 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { Prisma, User as UserModel } from '@prisma/client';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('user')
 export class UserController {
@@ -21,6 +23,7 @@ export class UserController {
     return this.userService.createUser(userData);
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   async getUserById(@Param('id') id: string): Promise<UserModel | null> {
     return this.userService.getUser({ id: Number(id) });
