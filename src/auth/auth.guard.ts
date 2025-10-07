e6b9eae61b33fa584602196ff2709eca632872bb
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-10-06 21:13:37 -0300

    fix: Ajusta payload do auth guard de sub para user

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -24,7 +24,7 @@ export class AuthGuard implements CanActivate {
           secret: process.env.SECRET_KEY,
         }
       );
-      request['sub'] = payload;
+      request['user'] = payload;
     } catch (error) {
       throw new UnauthorizedException('Unathorized')
     }
