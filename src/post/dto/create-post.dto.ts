29e61c134c77f3ca2b00782cf6f7326aa7d4beb2
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-10-08 19:54:44 -0300

    feat: Adiciona validação de tipos para Post

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, max, MaxLength } from "class-validator";
+
 export class CreatePostDto {
+    @IsNotEmpty()
+    @MaxLength(40)
     title: string;
+
+    @IsNotEmpty()
+    @MaxLength(255)
     content: string;
 }
